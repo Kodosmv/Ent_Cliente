d062352fec771642acbd787497d71623cfb5ca10
Kodosmv <soledat10@gmail.com>
2025-10-21 18:42:48 +0200

    21

---
+++ Eventos/ejercicios/ej1.js
@@ -0,0 +1,12 @@
+let newArray = [];
+function addElement(){
+   
+    let newNum =Math.floor(Math.random() * 10);
+    newArray.push(newNum);
+
+    let li = document.createElement("li");
+    li.textContent= newNum;
+    document.getElementById("lista").appendChild(li);
+
+}
+document.getElementById("but").onclick= addElement;
---
+++ Eventos/ejercicios/ej2.js
@@ -0,0 +1,14 @@
+        
+        
+function eventoFind(){        
+        (evento)=>{
+        if(evento.type=="keyup"){
+        alert(evento.key);
+        }
+        else if(evento.type=="click"){
+        alert(evento.clientX+" "+evento.clientY);
+        }
+        }
+        document.getElementById("miObjeto").onclick=mostrarMensaje;
+        document.onkeyup=mostrarMensaje;
+    }
