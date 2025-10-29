05eb1b02c3281493e466ffe41774fcc3906e26b2
Sole <soledat10@gmail.com>
2025-10-29 22:32:52 +0100

    29

--- Entregas/pracEventos/ej2.js
+++ Entregas/pracEventos/ej2.js
@@ -8,57 +8,51 @@ let bienContrasenya = false;
 // validar para poder enviar
 function validar() {
 
-  if (bienEmail && bienContrasenya) {
+    if (bienEmail && bienContrasenya) {
         boton.disabled = false;
         boton.classList.add('enabled');
-      } else {
+    }else {
         boton.disabled = true;
         boton.classList.remove('enabled');
-      }
+    }
 
 }
 
 // comprobar email
 email.onblur = function() {
-  if (!email.value.includes('@')) {
-    errorEmail.textContent = 'El email necesita @';
-    bienEmail = false;
-  } else {
-    errorEmail.textContent = '';
-    bienEmail = true;
-  }
-  validar();
+    if (!email.value.includes('@')) {
+        errorEmail.textContent = 'El email necesita @';
+        errorEmail.style.color='red';
+        bienEmail = false;
+    } else{
+        errorEmail.textContent = '';
+        bienEmail = true;
+    }
+    validar();
 };
 //vaciar error email
 email.oninput = function() {
-  errorEmail.textContent = '';
-  bienEmail = false;
-  validar();
+    errorEmail.textContent = '';
+    bienEmail = false;
+    validar();
 };
 
 // comprobar contraseña
 contrasenya.onblur = function() {
-  if (contrasenya.value.length < 8 || contrasenya.value.length > 10) {
-    errorContrasenya.textContent = 'La contraseña debe ser por lo menos 8-10 caracteres';
-    bienContrasenya = false;
-  } else {
-    errorContrasenya.textContent = '';
-    bienContrasenya = true;
-  }
-  validar();
+    if (contrasenya.value.length < 8 || contrasenya.value.length > 10) {
+        errorContrasenya.textContent = 'La contraseña debe tener  8-10 caracteres';
+        errorContrasenya.style.color='red';
+        bienContrasenya = false;
+    } else{
+        errorContrasenya.textContent = '';
+        bienContrasenya = true;
+    }
+    validar();
 };
 //vaciar error contra
 contrasenya.oninput = function() {
-  errorContrasenya.textContent = '';
-  bienContrasenya = false;
-  validar();
+    errorContrasenya.textContent = '';
+    bienContrasenya = false;
+    validar();
 };
 
-//envair fomr
-document.getElementById('formulario').addEventListener('submit', function(e) {
-  if (!(bienEmail && bienContrasenya)) {
-    e.preventDefault();
-  } else {
-    alert('Formulario enviado correctamente');
-  }
-});
